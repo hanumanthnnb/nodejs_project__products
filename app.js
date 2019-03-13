91c6bc87e7f24d5d7733b65ede49ce9a2930d224
Hanumanthnbalu <hanumanthnbalu@gmail.com>
2019-03-13 23:51:55 +0530

    nodejs MVC

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-    console.log(req);
-});
+const app = express();
+const path = require('path');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/404');
 
-server.listen(3000);
-teststytd
+const port = process.env.PORT | 3000;
+
+// Template Engines Setup
+app.set('view engine', 'ejs');
+app.set('views', "views/layouts");
+
+const rootDir = require('./utils/path');
+// Path which we can want to execute
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+
+// 404 page
+app.use(errorController.pageNotFound);
+
+
+app.listen(port);
+
+
+
+// .use allow us to add middleware function it accepts array of request handler
+// next() allows the request to continue to the next middleware in line
---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// COntrollers
+const productsController = require('../controllers/products');
+
+
+
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct); 
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct); 
+
+module.exports = router;
